ab9d8d9979683558a94fb182550ad0dafd3e13fd
Danila Spasov <spasov2000@mail.ru>
2026-08-28 17:17:59 +0300

    Configure Celery with Redis

--- config/settings.py
+++ config/settings.py
@@ -169,3 +169,19 @@ DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER", "noreply@library.local")
 PASSWORD_RESET_TIMEOUT = 60 * 60 * 24
 
 AUTH_USER_MODEL = "users.User"
+
+
+# Celery
+
+CELERY_BROKER_URL = os.getenv(
+    "CELERY_BROKER_URL",
+    "redis://localhost:6379/0",
+)
+CELERY_RESULT_BACKEND = os.getenv(
+    "CELERY_RESULT_BACKEND",
+    "redis://localhost:6379/1",
+)
+CELERY_ACCEPT_CONTENT = ("json",)
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TIMEZONE = TIME_ZONE
